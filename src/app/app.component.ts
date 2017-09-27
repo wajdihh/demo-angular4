7461ac240b45faeeb312b8ecfe77d0a81be14bc0
wajdihh <wajdihh@gmail.com>
2017-09-27 18:29:16 +0200

    hero demo until p2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
+import {HEROES} from './dump-hero';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  selectedHero : Hero;
+  heroes = HEROES;
+  hero: Hero = {
+    id: 1,
+    name: 'Windstrom'
+  };
+
+  onSelectHero(hero: Hero) : void {
+    this.selectedHero = hero;
+  }
 }
