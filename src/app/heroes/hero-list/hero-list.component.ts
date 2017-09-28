6a0183408b81bd0d4089b35f732ae78b4d8f710b
wajdihh <wajdihh@gmail.com>
2017-09-28 17:20:12 +0200

    add service

--- src/app/heroes/hero-list/hero-list.component.ts
+++ src/app/heroes/hero-list/hero-list.component.ts
@@ -1,26 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../shared/hero.model';
-import {HEROES} from '../shared/dump-hero';
+import {HeroService} from '../shared/hero.service';
 
 @Component({
   selector: 'app-hero-list',
   templateUrl: './hero-list.component.html',
-  styleUrls: ['./hero-list.component.css']
+  styleUrls: ['./hero-list.component.css'],
+  providers : [HeroService]
 })
+
 export class HeroListComponent implements OnInit {
 
-  ngOnInit(): void {
-  }
 
   selectedHero : Hero;
-  heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstrom'
-  };
+  heroes : Hero[];
+
+
+  constructor(private heroService: HeroService){
+
+  }
+
+  ngOnInit(): void {
+   this.loadHeroes();
+  }
 
   onSelectHero(hero: Hero) : void {
     this.selectedHero = hero;
+  }
+
+  loadHeroes() : void {
+    this.heroService.getHeroesSlowly().then(promisedHeroes => this.heroes= promisedHeroes);
   }
 
 }
---
+++ src/app/heroes/shared/hero.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HEROES} from '../shared/dump-hero';
+import {Hero} from "./hero.model";
+
+@Injectable()
+export class HeroService {
+
+  constructor() { }
+
+
+  // instantanetlly
+  getHeroes() : Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+
+  // After 2 seconds
+  getHeroesSlowly() : Promise<Hero[]> {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(this.getHeroes()),2000);
+    });
+  }
+}
