81a055d411eeac789265a2ffaa978bb2b7a19313
wajdihh <wajdihh@gmail.com>
2017-10-02 23:28:14 +0200

    remove unused files

--- src/app/heroes/shared/hero.service.ts
+++ src/app/heroes/shared/hero.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import {HEROES} from '../shared/dump-hero';
-import {Hero} from "./hero.model";
 import {HttpClient} from "@angular/common/http";
 import { environment } from '../../../environments/environment';
 @Injectable()
@@ -15,19 +13,5 @@ export class HeroService {
   getHeroesAsConfig() {
     return this.http.get(this.baseUrl + '/v1/others/regions');
   }
-  // instantanetlly
-  getHeroes() : Promise<Hero[]> {
-    return Promise.resolve(HEROES);
-  }
-  // After 2 seconds
-  getHeroesSlowly() : Promise<Hero[]> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(this.getHeroes()),2000);
-    });
-  }
 
-  getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-      .then(heroes => heroes.find(hero => hero.id === id));
-  }
 }
